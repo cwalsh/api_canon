e70e3535a421ff63fad7fc0942ac60c8d1629dae
Cameron Walsh <cwalsh2@au.westfield.com>
2013-04-16 12:17:13 +1000

    Fix layout to work better with twitter-bootstrap-rails

--- lib/api_canon/app/helpers/api_canon_view_helper.rb
+++ lib/api_canon/app/helpers/api_canon_view_helper.rb
@@ -1,21 +1,5 @@
 module ApiCanon
   module ApiCanonViewHelper
-    def api_canon_page_title
-      "ApiCanon - Api Documentation"
-    end
-
-    def api_canon_page_description
-      api_canon_page_title
-    end
-
-    def api_canon_project_name
-      "Api Canon"
-    end
-
-    def api_canon_page_author
-      "Cameron Walsh"
-    end
-
     if Rails.version.starts_with?('2')
       def content_for?(content_partial_name)
         instance_variable_get("@content_for_#{content_partial_name}")
